dc79d720352a2af9ce515994e671a63aeaced47d
Chaitanya Alluri <chaithualluri@gmail.com>
2025-02-19 16:05:11 +0530

    Update Login.jsx

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
     setError('');
     setIsLoading(true);
     try {
-      const response = await axios.post('https://e-valuation-portal-server.onrender.com/api/auth/login', {
+      const response = await axios.post('http://localhost:5000/api/auth/login', {
         email,
         password,
         userType
